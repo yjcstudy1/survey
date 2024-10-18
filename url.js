833aa1fe4b6f9e80a35ade43f715df319cca6308
yj <153605289+yjcstudy1@users.noreply.github.com>
2024-10-18 10:06:18 +0800

    Create url.js

---
+++ url.js
@@ -0,0 +1,14 @@
+/*
+ * 將要隨機派發的網址放入下方（不需排序）
+ *
+ * 並請注意：
+ * 1. 除最後一項外，其餘項目用逗號分隔
+ * 2. 網址請用引號包起來
+ */
+
+
+const urls = [
+	'https://docs.google.com/forms/d/e/1FAIpQLSeTmJplSbQY6_92Q9EtsTQiD16GqTYMNzi51Ug-nIriUcwNug/viewform',
+	'https://docs.google.com/forms/d/e/1FAIpQLSfBuQkqizv37U-oTFLv4JCryBLQL8SOhQn0ZbFtVm7JAC0pcw/viewform',
+        'https://docs.google.com/forms/d/e/1FAIpQLSdkIOQE0F1_i67EBt-CtWL2qmv37CQMZnS4BYMYDEXzKYcFqQ/viewform'
+];
